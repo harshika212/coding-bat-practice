29a7332ead6b11e724ac4201055487fb7163b867
harshika212 <harshikasingh212@gmail.com>
2026-03-26 01:35:22 +0530

    Create  biggerTwo.java

---
+++ biggerTwo.java
@@ -0,0 +1,27 @@
+
+// Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+
+
+// biggerTwo([1, 2], [3, 4]) → [3, 4]
+// biggerTwo([3, 4], [1, 2]) → [3, 4]
+// biggerTwo([1, 1], [1, 2]) → [1, 2]
+
+public int[] biggerTwo(int[] a, int[] b) {
+  int sumA=0;
+  int sumB=0;
+  for(int i = 0; i<a.length; i++){
+    sumA= sumA+a[i];
+  }
+  for(int j=0;j<b.length;j++){
+    sumB= sumB+b[j];
+  }
+  if(sumA > sumB){
+    return a;
+  }
+  else if(sumB>sumA){
+    return b;
+  }
+  else {
+    return a;
+  }
+}
